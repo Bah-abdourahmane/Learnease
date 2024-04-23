9122c071dd52091ff62ae9689bca89ee0644cf15
bah-abdourahmane <bahabdourahimane@gmail.com>
2024-04-23 19:58:44 +0100

    some changes

--- resources/views/public-pages/courses/show.blade.php
+++ resources/views/public-pages/courses/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="h-full bg-third text-white">
         {{-- banner --}}
-        <div class="h-[70vh] w-full pt-14 px-5" style="background: url('/images/course-4.jpg') no-repeat center/cover">
+        <div class="lg:h-[70vh] md:h-[40vh] h-[70vh] w-full pt-14 px-5" style="background: url('/images/course-4.jpg') no-repeat center/cover">
             {{-- left Test --}}
             <div class="lg:pl-20 flex flex-col justify-between h-full pb-10">
                 <div class="">
@@ -29,7 +29,7 @@
         {{-- Cours content details --}}
         <div class="flex justify-between  gap-5 flex-wrap p-5 lg:px-24 py-10">
             {{-- left --}}
-            <div class="h-fit lg:basis-[35%]">
+            <div class="h-fit w-full lg:basis-[35%]">
                 <h1 class="text-2xl mb-5">Chapitres</h1>
                 <div class="bg-[#22244d] px-[2px] rounded py-5 drop-shadow-md shadow-md text-sm">
                     {{-- chapitre 1 --}}
