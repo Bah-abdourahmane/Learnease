23f73f8b45dbca7c3cd2318494b6a0c99ae4ea80
bah-abdourahmane <bahabdourahimane@gmail.com>
2024-04-28 23:07:00 +0100

    api/index.api some changes bring

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+        $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
